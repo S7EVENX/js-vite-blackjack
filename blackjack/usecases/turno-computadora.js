6be878ec22e050fa1894462e770a161aafab9ffc
Steven Esparza <stevenesparza36@gmail.com>
2023-01-16 12:29:13 -0500

    First commit

---
+++ blackjack/usecases/turno-computadora.js
@@ -0,0 +1,47 @@
+import { crearCartaHTML } from "./crear-carta-html";
+import { pedirCarta } from "./pedir-carta";
+import { valorCarta } from "./valor-carta";
+
+// turno de la computadora
+/**
+ * 
+ * @param {Number} puntosMinimos
+ * @param {HTMLElement} puntosHTML  elemento html para mostrar los puntos
+ * @param {HTMLElement} divCartasComputadora  elemento html para mostrar las cartas de la computadora
+ * @param {Array<String>} deck
+ */
+export const turnoComputadora = ( puntosMinimos, puntosHTML, divCartasComputadora, deck = [] ) => {
+
+    if (!puntosMinimos) throw new Error('Puntos minimos son necesarios');
+    if (!puntosHTML) throw new Error('Elemento del HTML necesario');
+
+    let puntosComputadora = 0;
+
+    do {
+        const carta = pedirCarta( deck );
+
+        puntosComputadora = puntosComputadora + valorCarta( carta );
+        puntosHTML.innerText = puntosComputadora;
+        
+        //Funcion para crear carta en el HTML
+        const imgCarta = crearCartaHTML( carta );
+        divCartasComputadora.append( imgCarta );
+
+        if( puntosMinimos > 21 ) {
+            break;
+        }
+
+    } while(  (puntosComputadora < puntosMinimos)  && (puntosMinimos <= 21 ) );
+
+    setTimeout(() => {
+        if( puntosComputadora === puntosMinimos ) {
+            alert('Nadie gana :(');
+        } else if ( puntosMinimos > 21 ) {
+            alert('Computadora gana')
+        } else if( puntosComputadora > 21 ) {
+            alert('Jugador Gana');
+        } else {
+            alert('Computadora Gana')
+        }
+    }, 100 );
+}
